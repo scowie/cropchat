30305c2296a12a92f338edd65d4bb3cf6b9f0ff1
Stephen Cowie <cowiestephenjames@gmail.com>
2017-12-28 10:29:22 -0500

    added material design lite

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,17 @@
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import 'material-design-lite'
+
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+  @import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
+
 body {
   margin: 0;
 }
