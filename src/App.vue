9805d828053b392b5ec6153a24245880e1e3ade0
Stephen Cowie <cowiestephenjames@gmail.com>
2017-12-28 10:36:49 -0500

    added basic navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">CropChat</span>
+      </div>
     </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">CropChat</span>
+      <nav class="mdl-navigation">
+        <router-link class="mdl-navigation__link" to="/" @click.native="hideMenu">Home</router-link>
+        <router-link class="mdl-navigation__link" to="/post" @click.native="hideMenu">Post a picture</router-link>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <router-view></router-view>
+      </div>
     </main>
   </div>
 </template>
@@ -14,7 +24,13 @@
   import 'material-design-lite'
 
   export default {
-    name: 'app'
+    name: 'app',
+    methods: {
+      hideMenu: function () {
+        document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
+        document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
+      }
+    }
   }
 </script>
 
